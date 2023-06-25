0f9f03d1957cdd82daf8542ebfcb8d27adf97885
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-06-25 18:02:19 +0530

    changed layout and added form components

--- src/components/QRGenerator.tsx
+++ src/components/QRGenerator.tsx
@@ -1,93 +1,79 @@
 import React, { useRef, useState } from "react";
 import QRCode from "react-qr-code";
-
-const makeWhatsAppURL = (phone: string) => `http://wa.me/${phone}/`;
-const makePhoneURL = (phone: string) => `tel:${phone}`;
-
-const OPTIONS_MAP = {
-  0: "WhatsApp",
-  1: "Phone",
-  2: "Basic",
-};
+import WhatsappForm from "./forms/WhatsappForm";
+import UrlForm from "./forms/UrlForm";
+import PhoneForm from "./forms/PhoneForm";
+import ContactForm from "./forms/ContactForm";
 
 function QRGenerator() {
+  const [selectedOption, setSelectedOption] = useState<number>(0);
   const [QRURL, setQRURL] = useState<string>();
-  const formRef = useRef<HTMLFormElement>(null!);
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const option = Number(formRef.current.option.value);
-    if (
-      Number.isNaN(option) ||
-      !!formRef.current.inputUrl.current ||
-      formRef.current.inputUrl.current === ""
-    )
-      return;
-    switch (option) {
-      case 0:
-        setQRURL(() => makeWhatsAppURL(`91${formRef.current.inputUrl.value}`));
-        break;
-      case 1:
-        setQRURL(() => makePhoneURL(`+91${formRef.current.inputUrl.value}`));
-        break;
-      case 2:
-        setQRURL(formRef.current.inputUrl.value);
-        break;
-      default:
-    }
+  const [linkURL, setLinkURL] = useState<string>();
+
+  const changeURL = (url: string, isLink: boolean) => {
+    if (isLink) setLinkURL(url);
+    setQRURL(url);
   };
+
+  const options = [
+    <WhatsappForm setURL={changeURL} />,
+    <PhoneForm setURL={changeURL} />,
+    <ContactForm setURL={changeURL} />,
+    <UrlForm setURL={changeURL} />,
+  ];
+
   return (
-    <form
-      className="flex flex-col w-full max-w-sm gap-4"
-      ref={formRef}
-      onSubmit={handleSubmit}
-    >
-      <div className="flex items-center py-0.5 border-b border-teal-500 justify-evenly">
-        <label
-          className="flex gap-1 leading-[100%] items-end px-2 py-2 hover:bg-[rgba(104,104,104,0.1)] cursor-pointer"
-          htmlFor={OPTIONS_MAP[0]}
+    <div className="grid items-start justify-center h-[500px] w-[600px]">
+      <div className="flex gap-2 border-b border-teal-400 items-center justify-center pb-2">
+        <button
+          onClick={() => setSelectedOption(0)}
+          title="WhatsApp"
+          className={`btn w-14 h-14 grid items-center justify-center border-teal-500 border-2 rounded-md hover:bg-teal-200 hover:bg-opacity-10 ${
+            selectedOption === 0
+              ? "border-teal-400 bg-teal-200 bg-opacity-5 shadow-sm shadow-teal-200"
+              : ""
+          }`}
         >
-          {OPTIONS_MAP[0]}
-          <input
-            defaultChecked
-            type="radio"
-            id={OPTIONS_MAP[0]}
-            name="option"
-            value={0}
-          />
-        </label>
-        <label
-          className="flex gap-1 leading-[100%] items-end px-2 py-2 hover:bg-[rgba(104,104,104,0.1)] cursor-pointer"
-          htmlFor={OPTIONS_MAP[1]}
+          <img className="w-10" src="/Wahtsapp Icon.png" alt="" />
+        </button>
+
+        <button
+          onClick={() => setSelectedOption(1)}
+          title="Phone"
+          className={`btn w-14 h-14 grid items-center justify-center border-teal-500 border-2 rounded-md hover:bg-teal-200 hover:bg-opacity-10 ${
+            selectedOption === 1
+              ? "border-teal-400 bg-teal-200 bg-opacity-5 shadow-sm shadow-teal-200"
+              : ""
+          }`}
         >
-          {OPTIONS_MAP[1]}
-          <input type="radio" id={OPTIONS_MAP[1]} name="option" value={1} />
-        </label>
-        <label
-          className="flex gap-1 leading-[100%] items-end px-2 py-2 hover:bg-[rgba(104,104,104,0.1)] cursor-pointer"
-          htmlFor={OPTIONS_MAP[2]}
+          <img className="w-10" src="/Phone Icon.png" alt="" />
+        </button>
+
+        <button
+          onClick={() => setSelectedOption(2)}
+          title="Contact Card"
+          className={`btn w-14 h-14 grid items-center justify-center border-teal-500 border-2 rounded-md hover:bg-teal-200 hover:bg-opacity-10 ${
+            selectedOption === 2
+              ? "border-teal-400 bg-teal-200 bg-opacity-5 shadow-sm shadow-teal-200"
+              : ""
+          }`}
         >
-          {OPTIONS_MAP[2]}
-          <input type="radio" id={OPTIONS_MAP[2]} name="option" value={2} />
-        </label>
+          <img className="w-10" src="/Contact Card Icon.png" alt="" />
+        </button>
+
+        <button
+          onClick={() => setSelectedOption(3)}
+          title="URL"
+          className={`btn w-14 h-14 grid items-center justify-center border-teal-500 border-2 rounded-md hover:bg-teal-200 hover:bg-opacity-10 ${
+            selectedOption === 3
+              ? "border-teal-400 bg-teal-200 bg-opacity-5 shadow-sm shadow-teal-200"
+              : ""
+          }`}
+        >
+          <img className="w-10" src="/Web Icon.png" alt="" />
+        </button>
       </div>
-      <div className="flex items-center border-b border-teal-500 py-0.5">
-        <label className="flex justify-end gap-1" htmlFor="inputUrl">
-          URL:
-          <input
-            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 outline-none py-0.5 px-2 cursor-pointer"
-            name="inputUrl"
-            id="inputUrl"
-            type="text"
-            placeholder="Enter URL"
-          />
-        </label>
-      </div>
-      <button
-        className="flex-shrink-0 px-4 py-1 text-sm font-bold uppercase bg-teal-500 border-4 border-teal-500 rounded text-gray-950 hover:bg-teal-700 hover:border-teal-700"
-        type="submit"
-      >
-        generate
-      </button>
+      {options[selectedOption]}
       <div className="flex flex-col items-center justify-center gap-2 mt-2 qr-container">
         {QRURL && (
           <QRCode
@@ -99,11 +85,15 @@ function QRGenerator() {
             }}
           />
         )}
-        <a className="text-teal-600 border-b border-teal-500 cursor-pointer url hover:border-teal-300">
-          {QRURL}
+        <a
+          className="text-teal-600 border-b border-teal-500 cursor-pointer url hover:border-teal-300"
+          target="_blank"
+          href={linkURL}
+        >
+          {linkURL}
         </a>
       </div>
-    </form>
+    </div>
   );
 }
 
---
+++ src/components/forms/PhoneForm.tsx
@@ -0,0 +1,43 @@
+import React, { useRef } from "react";
+
+function PhoneForm({
+  setURL,
+}: {
+  setURL: (url: string, link: boolean) => void;
+}) {
+  const formRef = useRef<HTMLFormElement>(null!);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setURL(`tel:${formRef.current.inputUrl.value}`, true);
+  };
+
+  return (
+    <form
+      className="flex flex-col w-full max-w-sm gap-4"
+      ref={formRef}
+      onSubmit={handleSubmit}
+    >
+      <div className="flex items-center border-b border-teal-500 py-0.5">
+        <label className="flex justify-end gap-1" htmlFor="inputUrl">
+          URL:
+          <input
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 outline-none py-0.5 px-2 cursor-pointer active:text-teal-400 focus:text-teal-400"
+            required
+            name="inputUrl"
+            id="inputUrl"
+            type="tel"
+            placeholder="Enter URL"
+          />
+        </label>
+      </div>
+      <button
+        className="flex-shrink-0 px-4 py-1 text-sm font-bold uppercase bg-teal-500 border-4 border-teal-500 rounded text-gray-950 hover:bg-teal-700 hover:border-teal-700"
+        type="submit"
+      >
+        generate
+      </button>
+    </form>
+  );
+}
+export default PhoneForm;
